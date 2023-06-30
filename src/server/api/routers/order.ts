c8bcbe2265f3c3d095053447b8c6a628e0572f93
Marcin Lech <arcinech@gmail.com>
2023-06-30 22:00:41 +0200

    New procedures with order routes

--- src/server/api/routers/order.ts
+++ src/server/api/routers/order.ts
@@ -4,6 +4,14 @@ import {
   publicProcedure,
   protectedProcedure,
 } from "~/server/api/trpc";
+import { Prisma } from "@prisma/client";
+
+const defaultOrder = Prisma.validator<Prisma.OrderSelect>()({
+  id: true,
+  userId: true,
+  orderItems: true,
+  orderStatus: true,
+});
 
 export const orderRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
@@ -13,4 +21,52 @@ export const orderRouter = createTRPCRouter({
       },
     });
   }),
+
+  getById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.order.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  create: protectedProcedure
+    .input(z.object({
+      userId: z.string(),
+      orderItems: z.array(z.object({
+        productId: z.string(),
+        quantity: z.number(),
+        price: z.number(),
+      })),
+    }))
+    .mutation(({input , ctx}) => {
+      return ctx.prisma.order.create({
+        select: defaultOrder,
+        data: {
+          userId: input.userId,
+          orderItems: {
+            create: input.orderItems,
+          },
+        },
+      });
+    })
+
+    update: protectedProcedure
+    .input(z.object({
+      id: z.string(),
+      orderStatus: z.string(),
+    }))
+    .mutation(({input , ctx}) => {
+      return ctx.prisma.order.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          orderStatus: input.orderStatus,
+        },
+      });
+    }),
+
 });
